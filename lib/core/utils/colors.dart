a29bcde23148919842d0884fe2920a30dbebb12c
sayedabdul-aziz <sayedabdulaziz10@gmail.com>
2025-04-26 16:47:20 +0300

    add colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color(0xFF000000);
+  static const Color accent = Color(0xFF000000);
+}
